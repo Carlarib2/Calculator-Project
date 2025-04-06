ac7b2e511e41ad2d5c558a7b8a306c7ed03e00dd
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2025-04-06 20:53:12 +0100

    Initial GUI setup with input field

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,17 @@
 from tkinter import Tk, Entry, Button, StringVar
 
+class Calculator :
+    def __init__(self,master):
+        master.title("Calculator")
+        master.geometry('357x420+0+0')
+        master.config(bg='gray')
+        master.resizable(False,False)
 
+        self.equation=StringVar()
+        self.entry_value=''
+        Entry(width=17,bg='#fff',font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
+
+#Initialize the app
 root = Tk()
 root.mainloop()
 
