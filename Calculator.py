0912bb31b26fa00f63ed2764e56cbbe50b4d03b8
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2025-04-06 21:04:17 +0100

    Class Calculator finished set up

--- Calculator.py
+++ Calculator.py
@@ -11,6 +11,18 @@ class Calculator :
         self.entry_value=''
         Entry(width=17,bg='#fff',font=('Arial Bold',28),textvariable=self.equation).place(x=0,y=0)
 
+    def show (self,value):
+        self.entry_value+=str(value)
+        self.equation.set(self.entry_value)
+    
+    def clear(self):
+        self.entry_value=''
+        slef.equation.set(self.entry_value)
+    
+    def solve(self):
+        result=eval(self.entry_value)
+        self.equation.set(self.entry_value)
+
 #Initialize the app
 root = Tk()
 root.mainloop()
